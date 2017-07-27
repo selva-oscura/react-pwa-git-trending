461aa59f63c0a5547b0674c27f76989e944c3cfe
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-27 15:31:15 -0700

    created getDates method to get dates for today and one week ago

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import Nav from './Nav';
 import Main from './Main';
 
 class App extends Component {
+
+  getDates(){
+    let dates = {}, now = new Date();
+    dates.endDate = now.toISOString().slice(0,10);
+    dates.startDate = new Date(now.getTime() - 7 * 1000 * 60 * 60 * 24).toISOString().slice(0,10);
+    return dates;
+  }
+
   render() {
+    let dates = this.getDates();
+    console.log(dates);
     return (
       <div className="App">
         <Nav />
