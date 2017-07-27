0f4c32f6d291273b87aa024abf5a2b3c495634f0
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-27 16:42:00 -0700

    added cards to parameters, mapped cards to Card

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import Card from './Card';
 
-const Main = () => {
+const Main = ({cards}) => {
   return (
     <main className="Main container">
       <h5 className="header teal-text">Trending</h5>
       <div className="row trends">
-        <Card
-          title="Card Title"
-          lang="JavaScript"
-          starCount="299"
-          forkCount="100"
-          description="A set of best practices for JavaScript projects"
-          link="url-here"
-        />
+        {cards.map((card) => (
+          <Card
+            title={card.full_name}
+            lang={card.language}
+            starCount={card.stargazers_count}
+            forkCount={card.forks_count}
+            description={card.description}
+            link={card.html_url}
+          />
+      ))}
       </div>
     </main>
   );
