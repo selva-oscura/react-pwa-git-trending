a1197ab6baac0c1d1af7d3675c91bbbfb41eadc1
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-27 11:32:13 -0700

    imported and displayed Main stub

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './Nav';
+import Main from './Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Nav />
+        <Main />
       </div>
     );
   }
