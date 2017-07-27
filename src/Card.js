2068789650f214ded8ce161c0f635fb9c34d703f
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-27 12:27:22 -0700

    added received parameters to Cards and displayed those params in the component

--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({title, lang, starCount, forkCount, description, link}) => {
   return (
       <div className="col s12 m4 card-template">
       <div className="card horizontal">
         <div className="card-stacked">
           <div classNames="card-content white-text">
-            <span className="card-title">Card Title</span>
+            <span className="card-title">{title}</span>
             <div className="card-sub grey-text text-lighten-2">
-              <i className="material-icons">info</i><span className="card-lang"> JavaScript</span>
-              <i className="material-icons">star</i><span className="card-stars"> 299</span>
-              <i className="material-icons">assessment</i><span claclassName="card-forks"> 100</span>
+              <i className="material-icons">info</i><span className="card-lang"> {lang}</span>
+              <i className="material-icons">star</i><span className="card-stars"> {starCount}</span>
+              <i className="material-icons">assessment</i><span claclassName="card-forks"> {forkCount}</span>
             </div>
-            <p>A set of best practices for JavaScript projects</p>
+            <p>{description}</p>
           </div>
           <div className="card-action">
-            <a className="#" className="card-link">Visit Repo</a>
+            <a href={`https://github.com/${link}`} className="card-link">Visit Repo</a>
           </div>
         </div>
       </div>
