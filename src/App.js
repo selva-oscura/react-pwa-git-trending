9ad7974d0157685562ff9fa0ae6f4d2fff605f6f
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-27 16:31:43 -0700

    added props/state; imported API, made call to gitHubTrending, and updated state.results with that data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import api from './api';
 import Nav from './Nav';
 import Main from './Main';
 
 class App extends Component {
-
+  constructor(props){
+    super(props);
+    this.state = {
+      results: [],
+    };
+    let dates = this.getDates();
+    console.log(dates);
+    api.gitHubTrending(dates)
+      .then((res, err) => {
+        if(res){
+          console.log('res', res.data);
+          this.setState({results: res.data.items.slice(0,30)});
+        }
+        if(err){
+          console.log('err', err);
+        }
+      });
+  }
   getDates(){
     let dates = {}, now = new Date();
     dates.endDate = now.toISOString().slice(0,10);
@@ -13,8 +31,6 @@ class App extends Component {
   }
 
   render() {
-    let dates = this.getDates();
-    console.log(dates);
     return (
       <div className="App">
         <Nav />
