df552a4e561f559bc70550f8ef770a5424535793
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-27 12:10:22 -0700

    imported and displayed Card component; removed hard-coded card from Main;

--- src/Main.js
+++ src/Main.js
@@ -1,28 +1,12 @@
 import React from 'react';
+import Card from './Card';
 
 const Main = () => {
   return (
     <main className="Main container">
       <h5 className="header teal-text">Trending</h5>
       <div className="row trends">
-        <div className="col s12 m4 card-template">
-          <div className="card horizontal">
-            <div className="card-stacked">
-              <div classNames="card-content white-text">
-                <span className="card-title">Card Title</span>
-                <div className="card-sub grey-text text-lighten-2">
-                  <i className="material-icons">info</i><span className="card-lang"> JavaScript</span>
-                  <i className="material-icons">star</i><span className="card-stars"> 299</span>
-                  <i className="material-icons">assessment</i><span claclassName="card-forks"> 100</span>
-                </div>
-                <p>A set of best practices for JavaScript projects</p>
-              </div>
-              <div className="card-action">
-                <a className="#" className="card-link">Visit Repo</a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Card />
       </div>
     </main>
   );
