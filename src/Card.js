d79dca0086fdd83f88000812598d900e194bc138
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-27 15:29:01 -0700

    corrected className typos

--- src/Card.js
+++ src/Card.js
@@ -5,12 +5,12 @@ const Card = ({title, lang, starCount, forkCount, description, link}) => {
       <div className="col s12 m4 card-template">
       <div className="card horizontal">
         <div className="card-stacked">
-          <div classNames="card-content white-text">
+          <div className="card-content white-text">
             <span className="card-title">{title}</span>
             <div className="card-sub grey-text text-lighten-2">
               <i className="material-icons">info</i><span className="card-lang"> {lang}</span>
               <i className="material-icons">star</i><span className="card-stars"> {starCount}</span>
-              <i className="material-icons">assessment</i><span claclassName="card-forks"> {forkCount}</span>
+              <i className="material-icons">assessment</i><span className="card-forks"> {forkCount}</span>
             </div>
             <p>{description}</p>
           </div>
