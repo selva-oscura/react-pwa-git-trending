8249e26b58ac0c0143e5fab3334905070137c4c8
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-27 12:09:22 -0700

    created Card component (hardcoded content )

---
+++ src/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+      <div className="col s12 m4 card-template">
+      <div className="card horizontal">
+        <div className="card-stacked">
+          <div classNames="card-content white-text">
+            <span className="card-title">Card Title</span>
+            <div className="card-sub grey-text text-lighten-2">
+              <i className="material-icons">info</i><span className="card-lang"> JavaScript</span>
+              <i className="material-icons">star</i><span className="card-stars"> 299</span>
+              <i className="material-icons">assessment</i><span claclassName="card-forks"> 100</span>
+            </div>
+            <p>A set of best practices for JavaScript projects</p>
+          </div>
+          <div className="card-action">
+            <a className="#" className="card-link">Visit Repo</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
