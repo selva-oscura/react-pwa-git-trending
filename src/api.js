aecf0fb68fe9dd10a8d2a31c2c27c19578c30189
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-27 21:37:48 -0700

    made language an optional parameter for api call

--- src/api.js
+++ src/api.js
@@ -3,8 +3,14 @@ import axios from 'axios';
 const api = {
 	// github
 	// Trending
-	gitHubTrending: function(dates){
-    const apiCall = `https://api.github.com/search/repositories?q=created%3A%22${dates.startDate}+..+${dates.endDate}%22%20language%3Ajavascript&sort=stars&order=desc`;
+	gitHubTrending: function(dates, language){
+    // language = language || "javascript";
+    let apiCall = `https://api.github.com/search/repositories?q=created%3A%22${dates.startDate}+..+${dates.endDate}"`
+    if(language){
+      apiCall += `%20language%3A${language}`;
+    }
+    apiCall += `&sort=stars&order=desc`;
+    console.log('apiCall', apiCall)
     return axios
       .get(apiCall)
       .then((response) => {
