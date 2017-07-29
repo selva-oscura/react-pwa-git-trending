01206e0cd0f369e9a6baad3a0a88b7387b285fed
Carol St.Louis <1.selva.oscura@gmail.com>
2017-07-28 17:37:35 -0700

    created form with text field for language and submit button

--- src/GitForm.js
+++ src/GitForm.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
-const GitForm = () => {
+const GitForm = ({form, submitForm, updateFormState}) => {
   return (
     <div className="GitForm row">
-      Form goes here
+      <form>
+        <label htmlFor="language">
+          Language
+          <input
+            type="text"
+            className="col s12 m4"
+            id="language"
+            value={form.language}
+            placeholder="Language"
+            onChange={updateFormState}
+          />
+        </label>
+        <input
+          type="button"
+          className="teal"
+          value="Update"
+          onClick={submitForm}
+        />
+     </form>
+
     </div>
   );
 };
