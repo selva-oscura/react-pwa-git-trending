41775b913275ebe8ba00b6745bf6d71fe1b3df77
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-02 16:21:26 -0700

    added Search Term to form; wrapped form elements in divs and adjusted styling

--- src/GitForm.js
+++ src/GitForm.js
@@ -4,25 +4,43 @@ const GitForm = ({form, submitForm, updateFormState}) => {
   return (
     <div className="GitForm row">
       <form>
-        <label htmlFor="language">
-          Language
-          <input
-            type="text"
-            className="col s12 m4"
-            id="language"
-            value={form.language}
-            placeholder="Language"
-            onChange={updateFormState}
-          />
-        </label>
-        <input
-          type="button"
-          className="teal"
-          value="Update"
-          onClick={submitForm}
-        />
+        <div
+          className="col s12 m6"
+        >
+          <label htmlFor="searchTerm">Search Term(s)
+            <input
+              className="active"
+              type="text"
+              id="searchTerm"
+              value={form.searchTerm}
+              onChange={updateFormState}
+            />
+          </label>
+        </div>
+        <div
+          className="col s12 m6"
+        >
+          <label htmlFor="language">Language
+            <input
+              className="active"
+              type="text"
+              id="language"
+              value={form.language}
+              onChange={updateFormState}
+            />
+          </label>
+        </div>
+        <div className="col s12 m4 offset-m4">
+          <button
+            className="btn teal waves-effect waves-light"
+            type="submit"
+            name="action"
+            onClick={submitForm}
+          >
+            Update<i className="material-icons right">send</i>
+          </button>
+        </div>
      </form>
-
     </div>
   );
 };
