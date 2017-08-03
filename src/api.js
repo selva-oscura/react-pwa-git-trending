63fae45c12eea0f6ee91875a66dbdfa3be44d80c
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-02 17:38:00 -0700

    moved getDates function from App.js to api.js; converted gitHubTrending (trending only) to queryGithub to create query regardless of searchType, searchTerm(s), and language;

--- src/api.js
+++ src/api.js
@@ -1,23 +1,40 @@
 import axios from 'axios';
 
 const api = {
+  getDates: function(){
+    let dates = {}, now = new Date();
+    dates.endDate = now.toISOString().slice(0,10);
+    dates.startDate = new Date(now.getTime() - 7 * 1000 * 60 * 60 * 24).toISOString().slice(0,10);
+    return dates;
+  },
+
 	// github
-	// Trending
-	gitHubTrending: function(dates, language){
-    let apiCall = `https://api.github.com/search/repositories?q=created%3A%22${dates.startDate}+..+${dates.endDate}"`;
+  queryGithub: function(searchType, searchTerm, language){
+    console.log('searchType:', searchType, '- searchTerm:', searchTerm, '- language:', language);
+    let apiCall = "https://api.github.com/search/repositories?q=";
+    if(searchType === "trending"){
+      let dates = this.getDates();
+      apiCall += `created:"${dates.startDate}+..+${dates.endDate}"`;
+    }
+    if(searchTerm){
+      apiCall += `%20${searchTerm.split(" ").join("+")}`;
+    }
     if(language){
-      apiCall += `%20language%3A${language}`;
+      apiCall += `%20language:${language}`;
+    }
+    if(searchType === "top" && !searchTerm && !language){
+      apiCall += 'stars:>25000';
     }
     apiCall += '&sort=stars&order=desc';
     console.log('apiCall', apiCall);
     return axios
       .get(apiCall)
       .then((response) => {
-        console.log('gitHub Trending response', response);
+        console.log('api.queryGithub response', response);
         return response;
       })
       .catch((error) => {
-        console.log('gitHub Trending error', error);
+        console.log('api.queryGithub error', error);
         throw error;
       });
   }
