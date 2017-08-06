7900465c49b3000ee6e40918251f8e29633d38ef
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-05 20:49:31 -0700

    created component to add <wbr> tags to potential breakpoints in a text (used for urls, dir paths, other under-spaced content)

---
+++ src/BreakingText.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BreakingText = ({text}) => {
+  let possibleBreakPoints = [[0]],
+      str = text.split('');
+  str.forEach((char, i)=>{
+    if(['.', '/', '\\', '-', ':', '_'].includes(char)){
+      possibleBreakPoints[possibleBreakPoints.length-1].push(i+1);
+      possibleBreakPoints.push([i+1]);
+    };
+  });
+  possibleBreakPoints[possibleBreakPoints.length-1].push(text.length);
+  return (
+    <span>
+      {possibleBreakPoints.map((sections)=>(<span>{text.slice(sections[0],sections[1])}<wbr /></span>))}
+    </span>
+  );
+};
+
+BreakingText.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default BreakingText;
