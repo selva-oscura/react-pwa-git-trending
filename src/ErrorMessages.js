0c9d338c658318d8a6faf763a292add065aa973f
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-05 19:56:40 -0700

    added PropType validation to ErrorMessages; made error text white;

--- src/ErrorMessages.js
+++ src/ErrorMessages.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './ErrorMessages.css';
 import ErrorMessage from './ErrorMessage';
 
 const ErrorMessages = ({errors}) => {
 	return (
-		<div className="ErrorMessages row red darken-4">
-			<p col s12>
-				{errors.map((error, i) => <ErrorMessage key={i} error={error} />)}
-			</p>
+		<div className="ErrorMessages row">
+			<div className="col s12 red darken-4">
+				<p className="white-text">
+					{errors.map((error, i) => <ErrorMessage key={i} error={error} />)}
+				</p>
+			</div>
 		</div>
 	)
 }
+
+ErrorMessages.propTypes = {
+  errors: PropTypes.array.isRequired,
+};
 
 export default ErrorMessages;
