4dbe449ff703594fc75da7278b7b412feb307c6a
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-05 18:42:47 -0700

    import ErrorMessages css; added ErrorMessages styling with height animation (fires on initial display); added column specification to paragraph surrounding ErrorMessage components;

--- src/ErrorMessages.js
+++ src/ErrorMessages.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './ErrorMessages.css';
 import ErrorMessage from './ErrorMessage';
 
 const ErrorMessages = ({errors}) => {
 	return (
-		<div className="row red darken-4">
-			<p>
+		<div className="ErrorMessages row red darken-4">
+			<p col s12>
 				{errors.map((error, i) => <ErrorMessage key={i} error={error} />)}
 			</p>
 		</div>
