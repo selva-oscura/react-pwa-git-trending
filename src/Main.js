845f8a482afe7ecb125151c0a822709978239072
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-06 00:34:54 -0700

    imported SearchSummary and Cards components and supplied data to them; removed code for generating search summary and cards

--- src/Main.js
+++ src/Main.js
@@ -2,57 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import GitForm from './GitForm';
 import ErrorMessages from './ErrorMessages';
-import Card from './Card';
+import SearchSummary from './SearchSummary';
+import Cards from './Cards';
 
 const Main = ({errors, form, updateFormState, updateSearchType, cards, lastSearchParameters, lastUpdated, lastUpdatedLocal}) => {
-  const resultsText = () => {
-    let text = `${lastSearchParameters.searchType} results`;
-    if(lastSearchParameters.keyWords || lastSearchParameters.language){
-      text += " for ";
-    }
-    if(lastSearchParameters.keyWords){
-      text += "keyword";
-      if(lastSearchParameters.keyWords.includes(' ')){
-        text+="s";
-      }
-      text += `: ${lastSearchParameters.keyWords}`;
-    }
-    if(lastSearchParameters.keyWords && lastSearchParameters.language){
-      text += " and ";
-    }
-    if(lastSearchParameters.language){
-      text += `language: ${lastSearchParameters.language}`;
-    }
-    return text.toUpperCase();
-  }
   return (
     <main className="Main container">
+      
       <GitForm
         form={form}
         updateFormState={updateFormState}
         updateSearchType={updateSearchType}
       />
+
       { errors.length ? <ErrorMessages errors={errors} /> : null}
-      <div className="row">
-        <h5 className="header teal-text">{resultsText()}</h5>
-        <h6>Last Updated: {typeof lastUpdatedLocal === "string" ? lastUpdatedLocal : "never"}</h6>
-      </div>
-      <div className="row">
-        { cards && cards.length > 0 ? (
-          cards.map((card, i) => (
-            <Card
-              key={i}
-              title={card.full_name}
-              lang={card.language}
-              starCount={card.stargazers_count}
-              forkCount={card.forks_count}
-              description={card.description}
-              link={card.html_url}
-            />
-        ))) : (
-          <p>No results found</p>
-        )}
-      </div>
+      
+      <SearchSummary
+        lastSearchParameters={lastSearchParameters}
+        lastUpdated={lastUpdated}
+        lastUpdatedLocal={lastUpdatedLocal}
+      />
+
+      <Cards
+        cards={cards}
+      />
+
     </main>
   );
 };
@@ -62,7 +36,7 @@ Main.propTypes = {
   form: PropTypes.object.isRequired,
   updateFormState: PropTypes.func.isRequired,
   updateSearchType: PropTypes.func.isRequired,
-  cards: PropTypes.array.isRequired,
+  cards: PropTypes.array,
   lastUpdated: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   lastUpdatedLocal: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
