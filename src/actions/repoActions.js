37f3d4abefa131287db89dcd00c7f36ac6c60054
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-06 21:39:24 -0700

    removed references to state from loadRepos function (will need to eventually pass state-based params to function call, but this will need to wait); imported types from actionTypes;

--- src/actions/repoActions.js
+++ src/actions/repoActions.js
@@ -1,9 +1,15 @@
+import * as types from './actionTypes';
 import api from '../api/api';
+
+export function loadReposSuccess(res) {
+	return { type: types.LOAD_REPOS_SUCCESS, res};
+}
 
 export function loadRepos() {
 	return function(dispatch) {
-		let form = this.state.form;
-		return api.queryGitHub(form.searchType, form.keyWords, form.language)
+		// let form = this.state.form;
+		// return api.queryGitHub(form.searchType, form.keyWords, form.language)
+		return api.queryGitHub()
 		.then(res => {
 			dispatch(loadReposSuccess(res));
 		}).catch(error => {
