64963c1a34db6276d50f6a62175e09e143e69ae5
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-08 21:59:31 -0700

    added proper data to pass to reducer

--- src/actions/repoActions.js
+++ src/actions/repoActions.js
@@ -1,19 +1,28 @@
 import * as types from './actionTypes';
 import api from '../api/api';
 
-export function loadReposSuccess(res) {
-	return { type: types.LOAD_REPOS_SUCCESS, res};
+export function loadReposSuccess(repos) {
+	return { type: types.LOAD_REPOS_SUCCESS, repos};
+}
+
+const localDateTime = (time) => {
+  const pad = num => num < 10 ? `0${num}` : num;
+  return `${time.getFullYear()}-${pad(time.getMonth() + 1)}-${pad(time.getDate())}, ${pad(time.getHours())}:${pad(time.getMinutes())}`;
 }
 
 export function loadRepos() {
 	return function(dispatch) {
-		// let form = this.state.form;
-		// return api.queryGitHub(form.searchType, form.keyWords, form.language)
 		return api.queryGitHub()
 		.then(res => {
-			dispatch(loadReposSuccess(res));
+			let now = new Date();
+			dispatch(loadReposSuccess({
+				totalCount: res.data.total_count,
+				items: res.data.items,
+				lastUpdated: now.getTime(),
+				lastUpdatedLocal: localDateTime(now),
+			}));
 		}).catch(error => {
-			throw(error)
+			throw error;
 		});
 	}
 }
