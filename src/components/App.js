0c868973d5517f7066fea2a0fd526dc3607fcff6
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-09 20:23:56 -0700

    redux-ifying (removal of methods related to changing state for repos and errors, addition of call to repos and errors actions); change data shape (reflects shape shown in src/reducers/initialState)

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { debounce } from 'throttle-debounce';
+import { bindActionCreators } from 'redux';
+import * as repoActions from '../actions/repoActions';
+import * as errorsActions from '../actions/errorsActions';
 import './App.css';
 import api from '../api/api.js';
 import Nav from './Nav';
 import Main from './Main';
 
-
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      lastSearch: {
-        results: [],
-        parameters: {
-          searchType: 'top',
-          keyWords: '',
-          language: '',
-        },
-        lastUpdated: -Infinity,
-        lastUpdatedLocal: -Infinity,
-      },
-      form: {
-        searchType: 'top',
-        keyWords: '',
-        language: '',
-      },
-      errors: [
-      ],
-    };
+  constructor(props, context){
+    super(props, context);
     this.queryGitHub = this.queryGitHub.bind(this);
     this.debounceQueryGitHub = debounce(500, this.debounceQueryGitHub);
     this.updateFormState = this.updateFormState.bind(this);
     this.updateSearchType = this.updateSearchType.bind(this);
-    this.queryGitHub();
-  }
-  localDateTime(){
-    const now = new Date();
-    const pad = num => num < 10 ? `0${num}` : num;
-    return `${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(now.getDate())}, ${pad(now.getHours())}:${pad(now.getMinutes())}`;
+    // console.log('this.props.children', this.props.children);
+    // console.log('props.children', props.children);
+    // console.log('this.state', this.state)
   }
   queryGitHub(){
-    let form = this.state.form;
-    api.queryGitHub(form.searchType, form.keyWords, form.language)
+    let searchForm = this.state.searchForm || {searchType: undefined, keyWords: "", language: ""};
+    api.queryGitHub(searchForm.searchType, searchForm.keyWords, searchForm.language)
       .then((res) => {
+        // if(res.data){
+        //   console.log('total count:', res.data.total_count);
+        // }
         let results = res.data.items.map((item) => {
           const {full_name, language, stargazers_count, forks_count, description, html_url} = item;
           return {full_name, language, stargazers_count, forks_count, description, html_url};
           }
         );
-        let parameters = Object.assign({}, form);
-        let lastUpdated = new Date().getTime();
-        let lastUpdatedLocal = this.localDateTime();
-        this.setState({lastSearch: { results, parameters, lastUpdated, lastUpdatedLocal }});
+          // let parameters = Object.assign({}, form);
+          // this.setState({searchForm: { results, parameters }});
         let error_messages = document.getElementById('ErrorMessages');
         if (error_messages) {
           error_messages.className = error_messages.className.slice(0, -13) + " remove-error";
@@ -78,36 +60,91 @@ class App extends Component {
     this.queryGitHub();
   }
   updateFormState(e){
-    let form = this.state.form;
-    form[e.target.id] = e.target.value;
-    this.setState({ form });
+    let searchForm = this.state.searchForm;
+    searchForm[e.target.id] = e.target.value;
+    this.setState({ searchForm });
     this.debounceQueryGitHub();
   }
   updateSearchType(searchType){
-    let form = this.state.form;
-    form.searchType = searchType;
-    this.setState({ form });
+    let searchForm = this.state.searchForm;
+    searchForm.searchType = searchType;
+    this.setState({ searchForm });
     this.queryGitHub();
   }
+  componentWillMount(){
+    console.log('lastUpdated', this.props.state.repos.lastUpdated)
+    if(typeof this.props.state.repos.lastUpdated === "number"){
+      this.props.actions.repoActions.loadRepos()
+        .then(res => {
+          console.log('res from componentWillMount', res)
+          dispatch(updateErrorsSuccess({
+           removalInProgress: true,
+          }));
+          setTimeout(() => {
+           dispatch(updateErrorsSuccess({
+             removalInProgress: false, 
+             messages: [],
+           }))
+          }, 2000);
+        })
+        .catch(error => {
+          console.log('err from componentWillMount -- unable to access git', typeof error, error, '\n', String(error));
+          let messages = [];
+          if (error.message==="Network Error") {
+            messages.push("You appear to be offline.", "Please check your internet connection.");
+          }else if (error.response.data.message.includes("API rate limit exceeded")) {
+            messages.push("I'm sorry, but GitHub is rate limited and the limit has been exceeded.", "Please wait a minute before resubmitting the query.");
+          } else if (error.response && error.response.data.message) {
+            messages.push(error.response.data.message);
+          } else {
+            messages.push(String(error));
+          }
+          let errors = {
+           messages: messages,
+           removalInProgress: false,            
+          }
+          this.props.actions.errorsActions.updateErrors(errors);
+        });
+    }
+  }
   render() {
+    const state = this.props.state;
     return (
       <div className="App">
+
         <Nav
-          queryGitHub={this.queryGitHub}
-        />
+           queryGitHub={this.queryGitHub}
+         />
+
         <Main
-          errors={this.state.errors}
-          form={this.state.form}
+          errors={state.errors}
+          form={state.searchForm}
           updateFormState={this.updateFormState}
           updateSearchType={this.updateSearchType}
-          cards={this.state.lastSearch.results}
-          lastSearchParameters={this.state.lastSearch.parameters}
-          lastUpdated={this.state.lastSearch.lastUpdated}
-          lastUpdatedLocal={this.state.lastSearch.lastUpdatedLocal}
+          cards={state.repos.items}
+          lastSearchParameters={state.searchForm}
+          lastUpdated={state.repos.lastUpdated}
+          lastUpdatedLocal={state.repos.lastUpdatedLocal}
         />
+
       </div>
     );
   }
 }
 
-export default App;
+
+function mapStateToProps(state, ownProps){
+  console.log('mapStateToProps data', state, ownProps)
+  return { state: state };
+};
+
+function mapDispatchToProps(dispatch){
+  return {
+    actions: {
+      repoActions: bindActionCreators(repoActions, dispatch),
+      errorsActions: bindActionCreators(errorsActions, dispatch),
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
