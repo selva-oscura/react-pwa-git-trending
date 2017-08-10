0db2adeeed971a2cb69977eba4a5ccc790639717
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-09 19:37:49 -0700

    created initialState to pbootstrap state for reducers

---
+++ src/reducers/initialState.js
@@ -0,0 +1,17 @@
+export default {
+  repos: {
+    totalCount: 0,
+    items:[],
+    lastUpdated: -Infinity,
+    lastUpdatedLocal: -Infinity,
+  },
+  searchForm: {
+    searchType: 'top',
+    keyWords: '',
+    language: '',
+  },
+  errors: {
+    messages: [],
+    removalInProgress: false,
+  },
+}
