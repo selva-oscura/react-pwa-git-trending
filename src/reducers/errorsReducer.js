c6bf986d49560dee8c4db6a8a59a73be150b5cda
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-10 20:42:09 -0700

    created CLEAR_ERRORS_DISPLAY_SUCCESS and DELETE_ERRORS_SUCCESS

--- src/reducers/errorsReducer.js
+++ src/reducers/errorsReducer.js
@@ -2,9 +2,10 @@ import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
 export default function errorsReducer(state = initialState.errors, action) {
-	// console.log('state before update from errorsReducer', state)
 	switch(action.type) {
 		case types.UPDATE_ERRORS_SUCCESS:
+		case types.CLEAR_ERRORS_DISPLAY_SUCCESS:
+		case types.DELETE_ERRORS_SUCCESS:
 			return action.errors;
 		default:
 			return state;
