d62a08c6c4acd37dae2b44be876eb888e22af891
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-13 16:43:43 -0700

    added ajaxCalls to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import {combineReducers} from 'redux';
 import repos from './repoReducer';
 import searchForm from './searchFormReducer';
 import errors from './errorsReducer';
+import ajaxCalls from './ajaxCallsReducer';
 
 const rootReducer = combineReducers({
 	repos,
 	searchForm,
 	errors,
+	ajaxCalls,
 });
 
 export default rootReducer;
