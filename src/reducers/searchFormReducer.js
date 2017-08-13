5c0e979fb4c507f9d6778529de77be673d7214fc
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-12 23:48:57 -0700

    Object.assign to return searchForm data as new object

--- src/reducers/searchFormReducer.js
+++ src/reducers/searchFormReducer.js
@@ -2,10 +2,9 @@ import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
 export default function searchFormReducer(state = initialState.searchForm, action) {
-	// ;console.log('state before update from searchFormReducer', state);
 	switch(action.type) {
 		case types.UPDATE_SEARCH_FORM_SUCCESS:
-			return action.searchForm;
+			return Object.assign({}, action.searchForm);
 		default:
 			return state;
 	}
