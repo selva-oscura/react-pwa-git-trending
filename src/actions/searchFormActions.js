d56eff1ffe25f2af504f6cb9d6ccca02c36ca3af
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-12 23:47:05 -0700

    added searchForm as passed parameter; removed fake data and api import;

--- src/actions/searchFormActions.js
+++ src/actions/searchFormActions.js
@@ -1,18 +1,11 @@
 import * as types from './actionTypes';
-import api from '../api/api';
 
 export function updateSearchFormSuccess(searchForm) {
 	return { type: types.UPDATE_SEARCH_FORM_SUCCESS, searchForm};
 }
 
-export function updateSearchForm() {
+export function updateSearchForm(searchForm) {
 	return function(dispatch) {
-	  let searchForm = {
-	    searchType: 'top',
-	    keyWords: 'fake data',
-	    language: 'javascript',
-	  };
-
 		dispatch(updateSearchFormSuccess(searchForm));
-	}
-}
+	};
+};
