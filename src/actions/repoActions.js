14b83b8798553aef68b461fb2d1a24db0f8f00cd
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-12 22:22:33 -0700

    added seconds to localDateTime; passed searchType, keyWords, language to queryGitHub; pruned data in res.data.items passed to loadReposSuccess

--- src/actions/repoActions.js
+++ src/actions/repoActions.js
@@ -7,19 +7,25 @@ export function loadReposSuccess(repos) {
 
 const localDateTime = (time) => {
   const pad = num => num < 10 ? `0${num}` : num;
-  return `${time.getFullYear()}-${pad(time.getMonth() + 1)}-${pad(time.getDate())}, ${pad(time.getHours())}:${pad(time.getMinutes())}`;
+  return `${time.getFullYear()}-${pad(time.getMonth() + 1)}-${pad(time.getDate())}, ${pad(time.getHours())}:${pad(time.getMinutes())}:${pad(time.getSeconds())}`;
 }
 
-export function loadRepos() {
+export function loadRepos(searchType, keyWords, language) {
 	return function(dispatch) {
-		return api.queryGitHub()
+		return api.queryGitHub(searchType, keyWords, language)
 		.then(res => {
+			res.data.items = res.data.items.map((item) => {
+				const {full_name, language, stargazers_count, forks_count, description, html_url} = item;
+				return {full_name, language, stargazers_count, forks_count, description, html_url};
+			});
 			let now = new Date();
+			let lastUpdated = now.getTime();
+			let lastUpdatedLocal = localDateTime(now);
 			dispatch(loadReposSuccess({
 				totalCount: res.data.total_count,
 				items: res.data.items,
-				lastUpdated: now.getTime(),
-				lastUpdatedLocal: localDateTime(now),
+				lastUpdated: lastUpdated,
+				lastUpdatedLocal: lastUpdatedLocal,
 			}));
 		}).catch(error => {
 			throw error;
