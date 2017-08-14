301791b01cdfd3a5efbc2fcc3c3264b52d77faf3
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-13 20:33:05 -0700

    applied masonry.js (via react-masonry-component) to Card components

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Masonry from 'react-masonry-component';
 import Card from './Card';
 
 const Cards = ({ cards }) => {
+  const masonryOptions = {
+    transitionDuration: 0,
+  }
   return (
     <div className="section">
       <div className="row">
-        {cards && cards.length > 0
-          ? cards.map((card, i) =>
-              <Card
-                key={i}
-                title={card.full_name}
-                language={card.language}
-                starCount={card.stargazers_count}
-                forkCount={card.forks_count}
-                description={card.description}
-                link={card.html_url}
-              />
-            )
-          : <p>No results found</p>}
+        <Masonry 
+          options={masonryOptions}
+        >
+          {cards && cards.length > 0
+            ? cards.map((card, i) =>
+                <Card
+                  key={i}
+                  title={card.full_name}
+                  language={card.language}
+                  starCount={card.stargazers_count}
+                  forkCount={card.forks_count}
+                  description={card.description}
+                  link={card.html_url}
+                />
+              )
+            : <p>No results found</p>
+          }
+        </Masonry>
       </div>
     </div>
   );
