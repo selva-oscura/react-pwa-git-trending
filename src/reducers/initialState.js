a7e2c8173a85a5572dd207ffe2869b5002cac6b4
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-14 23:16:48 -0700

    add localStorage; made default value of initialState based upon localStorage data (if present)

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,25 +1,39 @@
-export default {
-  repos: {
-    totalCount: 0,
-    items:[],
-    lastUpdated: -Infinity,
-    lastUpdatedLocal: -Infinity,
-    lastQuery: {
-      searchType: '',
-      keyWords: '',
-      language: '',
-    },
-  },
-  searchForm: {
-    searchType: 'top',
-    keyWords: '',
-    language: '',
-  },
-  errors: {
-    messages: [],
-    removalInProgress: false,
-  },
-  ajaxCalls:{
-    callsInProgress: false,
+const initialState = () => {
+  const localStore = localStorage;
+  let initialState;
+  console.log('localStore', localStore);
+  if(localStore && localStore.gitUp){
+    console.log("localStorage and gitUp data in localStorage");
+    initialState = localStore.gitUp;
+  } else{
+    console.log("no localStorage or no gitUp data in localStorage");
+    initialState = {
+      repos: {
+        totalCount: 0,
+        items:[],
+        lastUpdated: -Infinity,
+        lastUpdatedLocal: -Infinity,
+        lastQuery: {
+          searchType: '',
+          keyWords: '',
+          language: '',
+        },
+      },
+      searchForm: {
+        searchType: 'top',
+        keyWords: '',
+        language: '',
+      },
+      errors: {
+        messages: [],
+        removalInProgress: false,
+      },
+      ajaxCalls:{
+        callsInProgress: false,
+      }
+    }
   }
+  return initialState;
 }
+
+export default initialState();
