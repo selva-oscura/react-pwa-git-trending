526f239135705b92551556753aad77319401b3ad
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-23 22:12:13 -0700

    added LOAD_SAVED_DATA_SUCCESS to actionTypes; commented each actionType to clarify purpose;

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,11 +1,20 @@
+// repos from API call to GitHub
 export const LOAD_REPOS_SUCCESS = 'LOAD_REPOS_SUCCESS';
 
+// saved data from LocalStorage when offline
+export const LOAD_SAVED_DATA_SUCCESS = 'LOAD_SAVED_DATA_SUCCESS';
+
+// update state of search form
 export const UPDATE_SEARCH_FORM_SUCCESS = 'UPDATE_SEARCH_FORM_SUCCESS';
 
+// populate errors array in state
 export const UPDATE_ERRORS_SUCCESS = 'UPDATE_ERRORS_SUCCESS';
 
+// intermediate state before deleting errors (used to animate Errors component)
 export const CLEAR_ERRORS_DISPLAY_SUCCESS = 'CLEAR_ERRORS_DISPLAY_SUCCESS';
 
+// deletion of errors
 export const DELETE_ERRORS_SUCCESS = 'DELETE_ERRORS_SUCCESS';
 
+// toggle Loading on and off based on AJAX call in progress / completed
 export const UPDATE_AJAX_CALLS_SUCCESS = 'UPDATE_AJAX_CALLS_SUCCESS';
