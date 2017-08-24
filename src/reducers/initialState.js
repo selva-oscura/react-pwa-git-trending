7213713889922805cbe22f2b62c21d4d6913a4da
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-23 21:57:34 -0700

    PWA-ification: pull repos and searchForm for initialState from localStorage.gitUp (if available) else default to first-time state

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,46 +1,46 @@
 const initialState = () => {
-  const localStore = localStorage;
-  let initialState;
-  console.log('localStore', localStore);
-  if(localStore && localStore.gitUp){
-    console.log("localStorage and gitUp data in localStorage");
-    const { repos, searchForm, errors, ajaxCalls } = localStorage.gitUp;
-    initialState = {
+  if(localStorage && localStorage.gitUp){
+    let gitUp = JSON.parse(localStorage.gitUp);
+    const { repos, searchForm } = gitUp;
+    repos.lastQuery = Object.assign({}, searchForm);
+    return {
       repos,
       searchForm,
-      errors,
-      ajaxCalls,
-    };
-  } else{
-    console.log("no localStorage or no gitUp data in localStorage");
-    initialState = {
-      repos: {
-        totalCount: 0,
-        items:[],
-        lastUpdated: -Infinity,
-        lastUpdatedLocal: -Infinity,
-        lastQuery: {
-          searchType: '',
-          keyWords: '',
-          language: '',
-        },
-      },
-      searchForm: {
-        searchType: 'top',
-        keyWords: '',
-        language: '',
-      },
       errors: {
         messages: [],
         removalInProgress: false,
       },
-      ajaxCalls:{
+      ajaxCalls: {
         callsInProgress: false,
-      }
+      },
+    };
+  }
+
+  return {
+    repos: {
+      totalCount: 0,
+      items:[],
+      lastUpdated: -Infinity,
+      lastUpdatedLocal: -Infinity,
+      lastQuery: {
+        searchType: '',
+        keyWords: '',
+        language: '',
+      },
+    },
+    searchForm: {
+      searchType: 'top',
+      keyWords: '',
+      language: '',
+    },
+    errors: {
+      messages: [],
+      removalInProgress: false,
+    },
+    ajaxCalls:{
+      callsInProgress: false,
     }
   }
-  console.log('basing initialState on localStorage', localStore && localStore.gitUp, '\ninitialState is', initialState);
-  return initialState;
 }
 
 export default initialState();
