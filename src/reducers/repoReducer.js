8c960e091517b06b1c221c21885d890892ee96d8
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-23 22:15:59 -0700

    PWA-ification: added LOAD_SAVED_DATA_SUCCESS

--- src/reducers/repoReducer.js
+++ src/reducers/repoReducer.js
@@ -4,8 +4,10 @@ import initialState from './initialState';
 export default function repoReducer(state = initialState.repos, action) {
 	switch(action.type) {
 		case types.LOAD_REPOS_SUCCESS:
+		case types.LOAD_SAVED_DATA_SUCCESS:
 			return Object.assign({}, action.repos);
 		default:
 			return state;
 	}
 };
+
